e78dada4cf74e8c40b06b421e860ee0e5b12fb3b
Aldair Segura Moreno <aldair.jsm@gmail.com>
2018-10-05 18:06:50 -0500

    Terminando sesion 2

--- SESION 2/examenPractico/app.js
+++ SESION 2/examenPractico/app.js
@@ -12,27 +12,36 @@ function mifuncion() {
     console.log("mi edad es: " + this.edad);
 }
 mifuncion();
-// // Cree una interfaz que sirva para validar el siguiente objeto
-// var batman = {
-//   nombre: "Bruno Díaz",
-//   artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
-// }
-// // Convertir esta funcion a una funcion de flecha
+var batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+};
+console.log("my name is: " + batman.nombre);
+console.log("my super power are: ");
+batman.artesMarciales.forEach(function (element) {
+    console.log(element);
+});
+// Convertir esta funcion a una funcion de flecha
 // function resultadoDoble( a, b ){
 //   return (a + b) * 2
 // }
-// // Función con parametros obligatorios, opcionales y por defecto
-// // donde NOMBRE = obligatorio
-// //       PODER  = opcional
-// //       ARMA   = por defecto = "arco"
-// function getAvenger( nombre, poder, arma ){
-//   var mensaje;
-//   if( poder ){
-//      mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
-//   }else{
-//      mensaje = nombre + " tiene un " + poder
-//   }
-// };
+var nume = function (a, b) { return (a + b) * 2; };
+console.log(nume(1, 2));
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre, poder, arma) {
+    if (arma === void 0) { arma = "arco"; }
+    var mensaje;
+    if (poder) {
+        mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+    }
+    else {
+        mensaje = nombre + " tiene un " + poder;
+    }
+}
+;
 // // Cree una clase que permita manejar la siguiente estructura
 // // La clase se debe de llamar rectangulo,
 // // debe de tener dos propiedades:
@@ -40,3 +49,11 @@ mifuncion();
 // //   * altura
 // // También un método que calcule el área  =  base * altura,
 // // ese método debe de retornar un numero.
+var rectangulo = /** @class */ (function () {
+    function rectangulo() {
+    }
+    rectangulo.prototype.calcularArea = function () {
+        return this.base * this.altura;
+    };
+    return rectangulo;
+}());
--- SESION 2/examenPractico/app.ts
+++ SESION 2/examenPractico/app.ts
@@ -1,17 +1,17 @@
 
 // Uso de Let y Const
-let nombre:String = "Ricardo Tapia";
-let edad:number = 23;
+let nombre: String = "Ricardo Tapia";
+let edad: number = 23;
 
-const PERSONAJE : {nombre:String , edad:number} = {
+const PERSONAJE: { nombre: String, edad: number } = {
   nombre: nombre,
   edad: edad
 };
 
 function mifuncion() {
 
-  let nombre:String =  "pedro";
-  let edad:number = 26;
+  let nombre: String = "pedro";
+  let edad: number = 26;
 
   console.log("mi nombre es: " + this.nombre);
   console.log("mi edad es: " + this.edad);
@@ -22,29 +22,48 @@ mifuncion();
 
 
 
-// // Cree una interfaz que sirva para validar el siguiente objeto
-// var batman = {
-//   nombre: "Bruno Díaz",
-//   artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
-// }
+// Cree una interfaz que sirva para validar el siguiente objeto
 
-// // Convertir esta funcion a una funcion de flecha
+//solucion 
+interface SuperHerue {
+  nombre: String,
+  artesMarciales: String[]
+}
+
+
+var batman: SuperHerue = {
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+}
+console.log("my name is: " + batman.nombre);
+console.log("my super power are: ");
+batman.artesMarciales.forEach(element => {
+  console.log(element);
+});
+
+
+// Convertir esta funcion a una funcion de flecha
 // function resultadoDoble( a, b ){
 //   return (a + b) * 2
 // }
 
-// // Función con parametros obligatorios, opcionales y por defecto
-// // donde NOMBRE = obligatorio
-// //       PODER  = opcional
-// //       ARMA   = por defecto = "arco"
-// function getAvenger( nombre, poder, arma ){
-//   var mensaje;
-//   if( poder ){
-//      mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
-//   }else{
-//      mensaje = nombre + " tiene un " + poder
-//   }
-// };
+let nume = (a: number, b: number) => (a + b) * 2;
+
+console.log(nume(1, 2));
+
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre: String, poder?: String, arma: String = "arco") {
+  var mensaje;
+  if (poder) {
+    mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+  } else {
+    mensaje = nombre + " tiene un " + poder
+  }
+};
 
 // // Cree una clase que permita manejar la siguiente estructura
 // // La clase se debe de llamar rectangulo,
@@ -53,3 +72,12 @@ mifuncion();
 // //   * altura
 // // También un método que calcule el área  =  base * altura,
 // // ese método debe de retornar un numero.
+class rectangulo {
+
+  base: number;
+  altura: number;
+
+  calcularArea():number{
+    return this.base * this.altura;
+  }
+}
